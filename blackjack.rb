467af541976ddd72743389e019e3e217bafae398
Kevin Meldau <me@kevinmeldau.com>
2021-04-25 19:01:26 -0400

    Player hand and dealer hand setup

--- blackjack.rb
+++ blackjack.rb
@@ -9,6 +9,14 @@ class BlackJack
         2, 3, 4, 5, 6, 7,8, 9, 10, 10, 10, 10, 11,
         2, 3, 4, 5, 6, 7,8, 9, 10, 10, 10, 10, 11]
     puts "Your card is #{get_random_card}"
+    @player_hand = get_starting_hand
+    @dealer_hand = get_starting_hand
+    puts "Player hand: #{@player_hand}"
+    puts "Dealer hand: #{@dealer_hand}"
+  end
+
+  def get_starting_hand
+    [get_random_card, get_random_card]
   end
 
   def get_random_card
